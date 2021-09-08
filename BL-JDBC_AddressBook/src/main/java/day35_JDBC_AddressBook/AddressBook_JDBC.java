50c7fb8e899dc5d74e8c3ec2e1fc4d4e5151c0d9
Aparna-Jadhav <jadhav.aparna19@gmail.com>
2021-09-08 23:50:32 +0530

    Adding the file of AdddressBook database connectivity problem solution

---
+++ BL-JDBC_AddressBook/src/main/java/day35_JDBC_AddressBook/AddressBook_JDBC.java
@@ -0,0 +1,212 @@
+package day35_JDBC_AddressBook;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AddressBook_JDBC 
+{
+	
+	public static void main(String[] args) 
+	{
+
+        boolean status = true;
+		while(status)
+		{
+			Scanner sc= new Scanner(System.in);
+			System.out.print("\n");
+	        System.out.print("\nDataBase Operation Menu ###");
+	        System.out.print("\n");
+	        System.out.print("\n1.Connection to contacts \n2.Read Contacts \n3.Update contacts  \n4.Count contacts \n5.Add new contact\n 6.Exit");
+	        System.out.print("\n\nChoose your option for DataBase Operation : ");
+	        int dbOption = sc.nextInt();
+	        switch(dbOption) {
+				case 1 : 
+					eshtablishConnection();
+					break;
+				case 2 : 
+					readAddressBook();
+					break;
+				case 3 : 
+					updateAddressBook();
+					break;
+				case 4 : 
+					countContactsByCity();
+					break;
+				case 5 : 
+					insertContact();
+					break;
+				case 6 :
+					status = false;
+					break;
+	        }
+        }	
+	}
+	
+	
+	public static void eshtablishConnection() {
+		Connection conn = getSqlConnection();
+		
+			System.out.println("Connection is eshtablished ");
+		
+	}
+
+	public static Connection getSqlConnection() {
+		Connection conn = null;
+		String dbHostUrl = "jdbc:mysql://localhost:3306/AddressBook_service";
+		String userName = "root";
+		String password = "root";
+		try {
+			conn = DriverManager.getConnection(dbHostUrl, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void readAddressBook() {
+		System.out.println("Displaying all data of AddressBook_service table");
+		Connection conn = getSqlConnection();
+
+		try {
+			if (conn != null) {
+				String readEmpPayroll = "SELECT * FROM AddressBook_service";
+				
+				java.sql.Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+					String firstName = resultSet.getString(1);
+					String lastName = resultSet.getString(2);
+					String adress = resultSet.getString(3);
+					String city = resultSet.getString(4);
+					String state = resultSet.getString(5);
+					Integer zip = resultSet.getInt(6);
+					String phoneNumber = resultSet.getString(7);
+					String email = resultSet.getString(8);
+				
+					String row = String.format(
+							"User record:\n FirstName: %s,\n LastName: %s, \n Address: %s,  \n City: %s,\n State: %s \n ",
+							firstName, lastName, adress, city, state, zip, phoneNumber, email);
+					System.out.println(row);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
+		}
+
+	}
+
+	public static void updateAddressBook() {
+		System.out.println("Updating address ");
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			String updateEmpPayroll = "UPDATE AddressBook_service SET address = ? WHERE lastName ='Shinde'";
+			try {
+				PreparedStatement preparedStatement = conn.prepareStatement(updateEmpPayroll);
+				preparedStatement.setString(1, "Jadhav");
+				int rowUpdated = preparedStatement.executeUpdate();
+				if (rowUpdated > 0) {
+					System.out.println("Data is Updated");
+				}
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			} finally {
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException sqlException) {
+						System.out.println(sqlException.getMessage());
+
+					}
+				}
+			}
+		}
+
+	}
+
+	
+	public static void countContactsByCity() {
+		System.out.println("Counting number of contact by city");
+		Connection conn = getSqlConnection();
+
+		try {
+			if (conn != null) {
+				String readEmpPayroll = "SELECT count(lastName) FROM AddressBook_service WHERE city = 'Solapur' or state ='Maharashtra'";
+
+				java.sql.Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+
+					int countContacts = resultSet.getInt(1);
+
+					String row = String.format("User record: \n Number of Contacts: %d", countContacts);
+					System.out.println(row);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
+		}
+
+	}
+
+	public static void insertContact() {
+		System.out.println("Inserting a new contact to AddressBook_service table");
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			String insertEmp = "INSERT INTO AddressBook_service (FirstName,LastName,Address,City,State,Zip,PhoneNum,EmailId) values(?,?,?,?,?,?,?,?)";
+			try {
+				PreparedStatement preparedStatement = conn.prepareStatement(insertEmp);
+				preparedStatement.setString(1, "Aishu");
+				preparedStatement.setString(2, "Mete");
+				preparedStatement.setString(3, "Akkalkot");
+				preparedStatement.setString(4, "Solapur");
+				preparedStatement.setString(5, "MH");
+				preparedStatement.setInt(6, 400002);
+				preparedStatement.setString(7, "9988776655");
+				preparedStatement.setString(8, "aishu@gmail");
+				
+				int rowUpdated = preparedStatement.executeUpdate();
+				if (rowUpdated > 0) {
+					System.out.println("Data is Updated");
+				}
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			} finally {
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException sqlException) {
+						System.out.println(sqlException.getMessage());
+
+					}
+				}
+			}
+		}
+
+	}
+}
